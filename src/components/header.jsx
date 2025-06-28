d9769a0458718d48b59aed9ec10ac4589c5ec4bb
Umer <omiomeromi11@gmail.com>
2025-06-28 15:40:04 +0300

    Make header fully responsive, but repititive code

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-// import MagnifyingGlass from './MagnifyingGlass';
 import HamburgerMenu from './hamburgerMenu';
 import { useState ,useEffect} from 'react';
 import CancelButton from './CancelButton';
 import SearchBar from './SearchBar';
 import NavLinks from './NavLinks';
+import MagnifyingGlass from './MagnifyingGlass';
+
 
 
 
@@ -42,16 +43,49 @@ const Header = () => {
       </div>
       {isMenuOpen && (
         <ul className="min-[500px]:hidden absolute top-full right-0 w-full bg-teal-800 z-40 p-4 shadow-lg">
-          <li>x</li>
-          <li>Y</li>
-          <li>Z</li>
-          <li>W</li>
+          <li>
+            <div className="md:w-[30%] flex items-center  self-end   relative order-1  col-span-2 ">
+              <input
+                type="search"
+                className="peer text-green-300 w-full px-4 py-2 rounded-lg border border-green-300 py-1 w-[3/4] focus:outline-none focus:ring-2 text-[13.5px] placeholder-green-200 focus:ring-emerald-400 focus:border-transparent transition duration-200 shadow-sm   appearance-none [&::-webkit-search-cancel-button]:hidden [&::-moz-search-clear-button]:hidden"
+                placeholder="Search for meals"
+              />
+              <button
+                type="submit"
+                className="absolute right-0 px-3 h-full bg-green-600 border border-green-300 border-l-0 peer-focus:border-transparent hover:bg-green-700 transition focus:bg-green-700
+    rounded-r-lg"
+              >
+                <MagnifyingGlass />
+              </button>
+            </div>
+          </li>
+          <ul className="navlinks md:order-2   flex flex-col space-x-4 *:text-green-200 *:hover:underline-offset-4 *:hover:underline *:hover:text-green-300 *:font-semibold self-center md:self-center   text-[0.9rem] content-end md:text-base justify-end *:active:text-green-700 *:py-2 *:hover:bg-green-300 ">
+            <li>
+              {" "}
+              <a className="transition-colors duration-300" href="">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="transition-colors duration-300" href="">
+                About
+              </a>
+            </li>
+            <li>
+              <a className="transition-colors duration-300" href="">
+                Contact
+              </a>
+            </li>
+            <li>
+              <a className="transition-colors duration-300" href="">
+                Login/Signup
+              </a>
+            </li>
+          </ul>
         </ul>
       )}
-      <SearchBar/>
-      <NavLinks/>
-     
-     
+      <SearchBar />
+      <NavLinks />
 
       <button
         onClick={() => toggleMenu()}
